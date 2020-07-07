bb2f1fd349fa1746c95ff19728b596fe944df3f4
CGessinger <christian@gessinger.de>
2020-07-07 10:20:34 +0200

    Setup workspace

---
+++ src/main/java/com/gessinger/onewiththejungle/OneWithTheJungle.java
@@ -0,0 +1,37 @@
+package com.gessinger.onewiththejungle;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("owtj")
+public class OneWithTheJungle
+{
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public OneWithTheJungle() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the doClientStuff method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event)
+    {
+    }
+}
