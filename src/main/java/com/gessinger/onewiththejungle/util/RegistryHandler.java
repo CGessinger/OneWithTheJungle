ffa8f0423dea6a202c23b0fd96b670661b82ac1a
CGessinger <christian@gessinger.de>
2020-07-07 15:20:45 +0200

    Add cow helmet, leggings and boots

--- src/main/java/com/gessinger/onewiththejungle/util/RegistryHandler.java
+++ src/main/java/com/gessinger/onewiththejungle/util/RegistryHandler.java
@@ -22,6 +22,15 @@ public class RegistryHandler
 	}
 
 	// Armor Item
+	public static final RegistryObject<ArmorItem> COW_HELMET = ITEMS.register("cow_helmet", () ->
+			new ArmorItem(OwtjArmorTier.COW_MATERIAL, EquipmentSlotType.HEAD, new Item.Properties().group(OneWithTheJungle.TAB)));
+
 	public static final RegistryObject<ArmorItem> COW_CHESTPLATE = ITEMS.register("cow_chestplate", () ->
 			new ArmorItem(OwtjArmorTier.COW_MATERIAL, EquipmentSlotType.CHEST, new Item.Properties().group(OneWithTheJungle.TAB)));
+
+	public static final RegistryObject<ArmorItem> COW_LEGGINGS = ITEMS.register("cow_leggins", () ->
+			new ArmorItem(OwtjArmorTier.COW_MATERIAL, EquipmentSlotType.LEGS, new Item.Properties().group(OneWithTheJungle.TAB)));
+
+	public static final RegistryObject<ArmorItem> COW_BOOTS = ITEMS.register("cow_boots", () ->
+			new ArmorItem(OwtjArmorTier.COW_MATERIAL, EquipmentSlotType.FEET, new Item.Properties().group(OneWithTheJungle.TAB)));
 }
