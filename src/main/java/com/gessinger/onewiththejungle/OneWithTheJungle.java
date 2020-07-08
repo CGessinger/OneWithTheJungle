472658da6bc1da51045781c6493051d5db836a1f
CGessinger <christian@gessinger.de>
2020-07-08 13:04:04 +0200

    Add cow armor effect
    Squashed commit of the following:
    
    commit 3e4c39035c6e975237971b52ae6d98f3a4deb230
    Author: CGessinger <christian@gessinger.de>
    Date:   Wed Jul 8 13:01:14 2020 +0200
    
        Add potion effect as argument
    
    commit d3914f22dfd157cc7f95ee4cf17c3f61d476b91f
    Author: CGessinger <christian@gessinger.de>
    Date:   Wed Jul 8 12:51:00 2020 +0200
    
        Change duration to 0
    
    commit fc554f08521c14770b99017a6f2ffe0eb6ef9f8f
    Author: CGessinger <christian@gessinger.de>
    Date:   Wed Jul 8 12:22:25 2020 +0200
    
        Use enum for potion effects
    
        Use enum class to not create new EffectInstances every time. Also use
        infinite duration to later on not update effect every tick.
    
    commit c1e0112f2a09b8298ed57be25d31f843568dbcad
    Author: CGessinger <christian@gessinger.de>
    Date:   Wed Jul 8 11:05:40 2020 +0200
    
        Give Resistance effect on full cow armor
    
    commit 3183a250fc7f25502962a966e58b7b650e23965d
    Author: CGessinger <christian@gessinger.de>
    Date:   Wed Jul 8 11:05:09 2020 +0200
    
        Update gitignore
    
    commit dfcc10529b46b96629f7dcd4977fdecd4147edcb
    Author: CGessinger <christian@gessinger.de>
    Date:   Tue Jul 7 23:56:19 2020 +0200
    
        cloud commit
    
    commit 363df35dcd4cbf238b21a054bd8e25e48837f20f
    Author: CGessinger <christian@gessinger.de>
    Date:   Tue Jul 7 23:43:31 2020 +0200
    
        unstable cloud commit

--- src/main/java/com/gessinger/onewiththejungle/OneWithTheJungle.java
+++ src/main/java/com/gessinger/onewiththejungle/OneWithTheJungle.java
@@ -3,7 +3,6 @@ package com.gessinger.onewiththejungle;
 import com.gessinger.onewiththejungle.util.RegistryHandler;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -11,8 +10,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("owtj")
--- src/main/java/com/gessinger/onewiththejungle/util/RegistryHandler.java
+++ src/main/java/com/gessinger/onewiththejungle/util/RegistryHandler.java
@@ -1,12 +1,12 @@
 package com.gessinger.onewiththejungle.util;
 
 import com.gessinger.onewiththejungle.OneWithTheJungle;
-import com.gessinger.onewiththejungle.items.ItemBase;
+import com.gessinger.onewiththejungle.effects.PotionEffects;
+import com.gessinger.onewiththejungle.items.armor.CustomArmorItem;
 import com.gessinger.onewiththejungle.items.armor.OwtjArmorTier;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,6 +15,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class RegistryHandler
 {
 	public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, OneWithTheJungle.MOD_ID);
+	private static final Item.Properties PROP_TAB = new Item.Properties().group(OneWithTheJungle.TAB);
 
 	public static void init ()
 	{
@@ -23,14 +24,14 @@ public class RegistryHandler
 
 	// Armor Item
 	public static final RegistryObject<ArmorItem> COW_HELMET = ITEMS.register("cow_helmet", () ->
-			new ArmorItem(OwtjArmorTier.COW_MATERIAL, EquipmentSlotType.HEAD, new Item.Properties().group(OneWithTheJungle.TAB)));
+			new CustomArmorItem(OwtjArmorTier.COW_MATERIAL, EquipmentSlotType.HEAD, PROP_TAB, "cow", PotionEffects.Resistance));
 
 	public static final RegistryObject<ArmorItem> COW_CHESTPLATE = ITEMS.register("cow_chestplate", () ->
-			new ArmorItem(OwtjArmorTier.COW_MATERIAL, EquipmentSlotType.CHEST, new Item.Properties().group(OneWithTheJungle.TAB)));
+			new ArmorItem(OwtjArmorTier.COW_MATERIAL, EquipmentSlotType.CHEST, PROP_TAB));
 
 	public static final RegistryObject<ArmorItem> COW_LEGGINGS = ITEMS.register("cow_leggings", () ->
-			new ArmorItem(OwtjArmorTier.COW_MATERIAL, EquipmentSlotType.LEGS, new Item.Properties().group(OneWithTheJungle.TAB)));
+			new ArmorItem(OwtjArmorTier.COW_MATERIAL, EquipmentSlotType.LEGS, PROP_TAB));
 
 	public static final RegistryObject<ArmorItem> COW_BOOTS = ITEMS.register("cow_boots", () ->
-			new ArmorItem(OwtjArmorTier.COW_MATERIAL, EquipmentSlotType.FEET, new Item.Properties().group(OneWithTheJungle.TAB)));
+			new ArmorItem(OwtjArmorTier.COW_MATERIAL, EquipmentSlotType.FEET, PROP_TAB));
 }
