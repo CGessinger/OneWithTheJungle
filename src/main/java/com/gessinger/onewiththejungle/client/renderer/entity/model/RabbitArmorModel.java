424addce4b52e2aa7aa8469708bfd5890d6efe73
CGessinger <christian@gessinger.de>
2020-07-29 18:59:35 +0200

    Update Rabbit armor model

--- src/main/java/com/gessinger/onewiththejungle/client/renderer/entity/model/RabbitArmorModel.java
+++ src/main/java/com/gessinger/onewiththejungle/client/renderer/entity/model/RabbitArmorModel.java
@@ -9,23 +9,24 @@ public class RabbitArmorModel extends BipedModel<LivingEntity>
 	public RabbitArmorModel (float modelSize)
 	{
 		super(modelSize, 0.0F, 64, 64);
-
 		textureWidth = 64;
 		textureHeight = 64;
 
 		ModelRenderer tail = new ModelRenderer(this);
 		tail.setRotationPoint(0.0F, 0.0F, 0.0F);
 		bipedBody.addChild(tail);
-		tail.setTextureOffset(8, 33).addBox(-2.0F, 7.0F, 2.0F, 4.0F, 4.0F, 3.0F, 0.0F, false);
+		tail.setTextureOffset(8, 33).addBox(-2.0F, 7.0F, 2.5F, 4.0F, 4.0F, 3.0F, -0.2F, false);
 
 		ModelRenderer leftEar = new ModelRenderer(this);
-		leftEar.setRotationPoint(3.5F, -7.5F, -0.5F);
+		leftEar.setRotationPoint(2.0F, -7.5F, -0.5F);
 		bipedHead.addChild(leftEar);
+		setRotationAngle(leftEar, 0.0F, -1.309F, 0.0F);
 		leftEar.setTextureOffset(0, 32).addBox(-0.5F, -5.5F, -1.5F, 1.0F, 5.0F, 3.0F, 0.0F, false);
 
 		ModelRenderer rightEar = new ModelRenderer(this);
-		rightEar.setRotationPoint(-3.5F, -7.5F, -0.5F);
+		rightEar.setRotationPoint(-2.0F, -7.5F, -0.5F);
 		bipedHead.addChild(rightEar);
+		setRotationAngle(rightEar, 0.0F, 1.309F, 0.0F);
 		rightEar.setTextureOffset(0, 32).addBox(-0.5F, -5.5F, -1.5F, 1.0F, 5.0F, 3.0F, 0.0F, true);
 	}
 
